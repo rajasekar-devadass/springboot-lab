f4e76dc5ec74fdb4129aae2bae60cdbc9a84a25b
Rajasekar <d.rajasekar02@gmail.com>
2023-12-23 13:08:19 +0530

    renamed folders

--- Day8-lab02-consumeRest-openfeign/library-rest/src/main/java/com/example/StudentController.java
+++ Day8-lab02-consumeRest-openfeign/library-rest/src/main/java/com/example/StudentController.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,24 +10,30 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentController {
 
+    private int studentCountForID;
     private final List<Student> studentList;
 
     public StudentController() {
         studentList = new LinkedList<>();
+        studentCountForID = 0;
     }
 
     @PostMapping("/")
+    //O(1)
     public Student add(@RequestBody Student student) {
-        student.setId(studentList.size());
+        studentCountForID++;
+        student.setId(studentCountForID);
         studentList.add(student);
         return student;
     }
 
     @PutMapping("/")
+    //O(n)
+    //HashMap - O(1)
     public Student put(@RequestBody Student inputStudent) {
-        for(int index = 0; index < studentList.size(); index++){
+        for (int index = 0; index < studentList.size(); index++) {
             Student studentObj = studentList.get(index);
-            if(studentObj.getId() == inputStudent.getId()){
+            if (studentObj.getId() == inputStudent.getId()) {
                 studentList.set(index, inputStudent);
                 return inputStudent;
             }
@@ -38,9 +43,9 @@ public class StudentController {
 
     @PatchMapping("/")
     public Student patch(@RequestBody Student inputStudent) {
-        for(int index = 0; index < studentList.size(); index++){
+        for (int index = 0; index < studentList.size(); index++) {
             Student studentObj = studentList.get(index);
-            if(studentObj.getId() == inputStudent.getId()){
+            if (studentObj.getId() == inputStudent.getId()) {
 //                studentList.set(index, inputStudent);
                 studentObj.setName(inputStudent.getName());
                 return inputStudent;
@@ -51,8 +56,8 @@ public class StudentController {
 
     @GetMapping("/{id}")
     public Student get(@PathVariable int id) {
-        for(Student studentObj : studentList){
-            if(studentObj.getId() == id){
+        for (Student studentObj : studentList) {
+            if (studentObj.getId() == id) {
                 return studentObj;
             }
         }
@@ -65,13 +70,12 @@ public class StudentController {
     }
 
 
-
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         Iterator<Student> iterator = studentList.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Student obj = iterator.next();
-            if(obj.getId() == id){
+            if (obj.getId() == id) {
                 iterator.remove();
             }
         }
